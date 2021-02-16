bacae4a9b1f95bedbaa02c198f07a9be38bdec9a
Serghei Sergheev <47123121+sergheevdev@users.noreply.github.com>
2021-02-16 22:59:20 +0100

    Added our own bootstrap template
    This template is personal, and can't be found anywhere, the organisation from scratch and the clean html produced will help us to migrate to Angular in the future.

---
+++ frontend/marketplace/js/main.js
@@ -0,0 +1,54 @@
+$(document).ready(() => {
+
+    registerNavigation();
+    registerHighlightEffect();
+
+});
+
+const deviceSize = () => {
+    return $('#screen-size-detector').find('div:visible').first().attr('id');
+}
+
+const registerNavigation = () => {
+
+    const deviceNavigationStyleSwitcher = () => {
+        const size = deviceSize();
+        if(size === "lg" || size === "xl" || size === "md") {
+            $("#menu-products-mobile:not([class*='d-none'])").addClass("d-none");
+            $("#menu-products-desktop:is([class*='d-none'])").removeClass("d-none");
+            
+        } else {
+            $("#menu-products-desktop:not([class*='d-none'])").addClass("d-none");
+            $("#menu-products-mobile:is([class*='d-none'])").removeClass("d-none");
+        }
+    }
+    $(window).on('load', deviceNavigationStyleSwitcher);
+    $(window).on('resize', deviceNavigationStyleSwitcher);
+
+}
+
+const registerHighlightEffect = () => {
+
+    const registerHighlight = (element) => {
+
+        const getColorForEvent = (eventName) => {
+            const color = "rgba(255, 255, 255, {opacity})";
+            const opacity = eventName === "mouseenter" ? "0.1" : "0.025";
+            return color.replace("{opacity}", opacity);
+        };
+
+        const performColorSwitch = (eventName) => {
+            element.css("background-color", getColorForEvent(eventName));
+        };
+
+        element
+            .on("mouseenter", (event) => performColorSwitch(event.type, element))
+            .on("mouseleave", (event) => performColorSwitch(event.type, element));
+    };
+
+    $(".highlight").each(function() { registerHighlight($(this)); });
+    
+}
+
+
+
