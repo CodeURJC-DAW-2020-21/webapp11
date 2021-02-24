39fdb9cda548ed7d90f04df1574d2e3ee806e6be
Serghei Sergheev <47123121+sergheevdev@users.noreply.github.com>
2021-02-24 23:03:59 +0100

    Added loading more animation in main.js

--- frontend/marketplace/js/main.js
+++ frontend/marketplace/js/main.js
@@ -3,6 +3,7 @@ $(document).ready(() => {
     registerNavigation();
     registerHighlightEffect();
     registerLoginToggler();
+    registerLoadMoreAnimation();
 
 });
 
@@ -75,5 +76,29 @@ const registerLoginToggler = () => {
         }
         updateLoginStatus();
     });
+
+}
+
+const registerLoadMoreAnimation = () => {
+
+    $(".load-more-button").each(function(){
+        const currentButton = $(this);
+        console.log("hi");
+
+        currentButton.click(() => {
+            currentButton.addClass("d-none");
+            const contentId = currentButton.data('daw-loading-element');
+            $("#" + contentId).removeClass("d-none");
+
+            const addButtonBack = () => {
+                currentButton.removeClass("d-none");
+                $("#" + contentId).addClass("d-none");
+            }
+
+            setTimeout(() => addButtonBack(), 1000);
+            
+        });
+    });
+    
 
 }
