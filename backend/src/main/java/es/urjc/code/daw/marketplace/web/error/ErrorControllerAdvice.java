8d72a08e48222547627262e26b6d78cf2c640be2
Serghei Sergheev <47123121+sergheevdev@users.noreply.github.com>
2021-03-09 19:10:10 +0100

    Restructured error handling

--- backend/src/main/java/es/urjc/code/daw/marketplace/web/error/ErrorControllerAdvice.java
+++ backend/src/main/java/es/urjc/code/daw/marketplace/web/error/ErrorControllerAdvice.java
@@ -1,4 +1,4 @@
-package es.urjc.code.daw.marketplace.web.user.controller;
+package es.urjc.code.daw.marketplace.web.error;
 
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.ModelAndView;
 
 @ControllerAdvice
-public class UserControllerAdvice {
+public class ErrorControllerAdvice {
 
     @ExceptionHandler(value = Exception.class)
     public ModelAndView handleError(Model model, Exception exception) {
