a9a514205572c73f53b9ea94718366221bca0c68
Serghei Sergheev <47123121+sergheevdev@users.noreply.github.com>
2021-03-30 13:00:54 +0200

    Added json properties to find user model

--- backend/src/main/java/es/urjc/code/daw/marketplace/api/user/dto/FindUserResponseDto.java
+++ backend/src/main/java/es/urjc/code/daw/marketplace/api/user/dto/FindUserResponseDto.java
@@ -1,5 +1,6 @@
 package es.urjc.code.daw.marketplace.api.user.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
 
 @Getter
@@ -9,16 +10,22 @@ import lombok.*;
 @NoArgsConstructor
 public class FindUserResponseDto {
 
+    @JsonProperty("id")
     private Long id;
 
+    @JsonProperty("first_name")
     private String firstName;
 
+    @JsonProperty("surname")
     private String surname;
 
+    @JsonProperty("address")
     private String address;
 
+    @JsonProperty("email")
     private String email;
 
+    @JsonProperty("enabled")
     private Boolean isEnabled = true;
 
 }
