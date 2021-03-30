b48e7cbb1b602f8a65c48cdb2bbf040e0c5af1b3
Serghei Sergheev <47123121+sergheevdev@users.noreply.github.com>
2021-03-30 18:01:43 +0200

    Added skeleton structure for REST controllers

---
+++ backend/src/main/java/es/urjc/code/daw/marketplace/api/order/controller/OrderRestController.java
@@ -0,0 +1,10 @@
+package es.urjc.code.daw.marketplace.api.order.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class OrderRestController {
+
+    // Implement me...
+
+}
