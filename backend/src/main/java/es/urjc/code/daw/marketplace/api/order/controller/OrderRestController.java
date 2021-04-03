232ee3b7f3d1966ac61d91fc8f62a5cfe06d2c07
Allan <allanmastercobb@gmail.com>
2021-04-03 18:56:29 +0200

    Added findServices methord
    Added the findServices method plus an additional private method called loggedUserFromToken.

--- backend/src/main/java/es/urjc/code/daw/marketplace/api/order/controller/OrderRestController.java
+++ backend/src/main/java/es/urjc/code/daw/marketplace/api/order/controller/OrderRestController.java
@@ -2,14 +2,21 @@ package es.urjc.code.daw.marketplace.api.order.controller;
 
 import es.urjc.code.daw.marketplace.api.order.dto.FindOrderResponseDto;
 import es.urjc.code.daw.marketplace.api.order.mapper.RestOrderMapper;
+import es.urjc.code.daw.marketplace.domain.Order;
 import es.urjc.code.daw.marketplace.domain.User;
 import es.urjc.code.daw.marketplace.security.jwt.JwtTokenService;
 import es.urjc.code.daw.marketplace.security.jwt.extractor.TokenExtractor;
 import es.urjc.code.daw.marketplace.service.OrderService;
 import es.urjc.code.daw.marketplace.service.PdfExporterService;
 import es.urjc.code.daw.marketplace.service.UserService;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 public class OrderRestController {
@@ -38,8 +45,27 @@ public class OrderRestController {
     }
 
 
+    public ResponseEntity<List<FindOrderResponseDto>> findServices(@RequestParam("page") Integer page,
+                                                                   @RequestParam("amount") Integer amount) {
 
+        User loggedUser = loggedUserFromToken();
+        List<Order> orders = orderService.findAllOrdersByUserId(loggedUser.getId(), PageRequest.of(page-1, amount));
+        List<FindOrderResponseDto> response = orders.stream().map(restOrderMapper::asFindResponse).collect(Collectors.toList());
 
+        return ResponseEntity.ok(response);
+    }
+
+    private User loggedUserFromToken() {
+        String token = tokenExtractor.containsToken() ? tokenExtractor.extractToken() : StringUtils.EMPTY;
+        String email = tokenService.extractTokenSubject(token);
+        User loggedUser = userService.findUserByEmail(email);
+        if (loggedUser == null){
+            throw new RuntimeException("Access denied");
+        } else {
+            return loggedUser;
+        }
+
+    }
 
 
 }
