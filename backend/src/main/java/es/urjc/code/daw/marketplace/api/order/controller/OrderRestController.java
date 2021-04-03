7b5ee8cbc7e9c64130dfdde9db86435f1284ff3b
Allan <allanmastercobb@gmail.com>
2021-04-03 18:34:43 +0200

    Implemented Order Mapper and FindOrderResponseDto
    Implemented OrderMapper, FindOrderResponseDto and created the builder for the OrderController.

--- backend/src/main/java/es/urjc/code/daw/marketplace/api/order/controller/OrderRestController.java
+++ backend/src/main/java/es/urjc/code/daw/marketplace/api/order/controller/OrderRestController.java
@@ -1,10 +1,45 @@
 package es.urjc.code.daw.marketplace.api.order.controller;
 
+import es.urjc.code.daw.marketplace.api.order.dto.FindOrderResponseDto;
+import es.urjc.code.daw.marketplace.api.order.mapper.RestOrderMapper;
+import es.urjc.code.daw.marketplace.domain.User;
+import es.urjc.code.daw.marketplace.security.jwt.JwtTokenService;
+import es.urjc.code.daw.marketplace.security.jwt.extractor.TokenExtractor;
+import es.urjc.code.daw.marketplace.service.OrderService;
+import es.urjc.code.daw.marketplace.service.PdfExporterService;
+import es.urjc.code.daw.marketplace.service.UserService;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class OrderRestController {
 
-    // Implement me...
+    private static final String ROOT_ROUTE = "api/orders";
+    private final UserService userService;
+    private final JwtTokenService tokenService;
+    private final TokenExtractor tokenExtractor;
+    private final OrderService orderService;
+    private final RestOrderMapper restOrderMapper;
+    private final PdfExporterService pdfExporterService;
+
+
+    public OrderRestController(UserService userService,
+                               JwtTokenService tokenService,
+                               TokenExtractor tokenExtractor,
+                               OrderService orderService,
+                               RestOrderMapper restOrderMapper,
+                               PdfExporterService pdfExporterService) {
+        this.userService = userService;
+        this.tokenService = tokenService;
+        this.tokenExtractor = tokenExtractor;
+        this.orderService = orderService;
+        this.restOrderMapper = restOrderMapper;
+        this.pdfExporterService = pdfExporterService;
+    }
+
+
+
+
+
 
 }
