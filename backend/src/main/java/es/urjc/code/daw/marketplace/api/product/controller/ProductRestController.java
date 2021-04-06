9cc88df2f7e9866a7562361b0bf0019e32bccd30
Alexrguez9 <72986293+Alexrguez9@users.noreply.github.com>
2021-04-06 10:07:54 +0200

    Documented with open api

--- backend/src/main/java/es/urjc/code/daw/marketplace/api/product/controller/ProductRestController.java
+++ backend/src/main/java/es/urjc/code/daw/marketplace/api/product/controller/ProductRestController.java
@@ -1,5 +1,6 @@
 package es.urjc.code.daw.marketplace.api.product.controller;
 
+import es.urjc.code.daw.marketplace.api.jwt.dto.GenerateTokenResponseDto;
 import es.urjc.code.daw.marketplace.api.product.dto.FindProductResponseDto;
 import es.urjc.code.daw.marketplace.api.product.dto.PlaceOrderResponseDto;
 import es.urjc.code.daw.marketplace.api.product.mapper.RestProductMapper;
@@ -9,6 +10,12 @@ import es.urjc.code.daw.marketplace.domain.User;
 import es.urjc.code.daw.marketplace.security.auth.AuthenticationService;
 import es.urjc.code.daw.marketplace.service.*;
 import es.urjc.code.daw.marketplace.util.EmailMessageFactory;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -44,15 +51,63 @@ public class ProductRestController {
         this.authenticationService = authenticationService;
     }
 
+    @Operation(summary = "Finds a list of all products")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "The list of products was found succesfully",
+                    content = {@Content(
+                            array = @ArraySchema(schema = @Schema(implementation = FindProductResponseDto.class))
+                    )}
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "Can not access to find products",
+                    content = @Content
+            ),
+
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "There were no more products to be shown",
+                    content = @Content
+            ),
+
+    })
+
     @RequestMapping(
             path = BASE_ROUTE,
             method = RequestMethod.GET
     )
     public ResponseEntity<List<FindProductResponseDto>> findAllProducts() {
         List<Product> products = productService.findAllProducts();
+        if(products.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
         List<FindProductResponseDto> response = products.stream().map(restProductMapper::asFindResponse).collect(Collectors.toList());
         return ResponseEntity.ok(response);
     }
+
+
+    @Operation(summary = "Finds an order with all the user´s products")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "The order from the user was found succesfully",
+                    content = {@Content(
+                            schema = @Schema(implementation = PlaceOrderResponseDto.class)
+                    )}
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "Cannot perform placing order",
+                    content = @Content
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Cannot perform the order because the product does not exist",
+                    content = @Content
+            ),
+    })
 
     @RequestMapping(
             path = BASE_ROUTE + "/{id}/purchase",
@@ -61,6 +116,9 @@ public class ProductRestController {
     public ResponseEntity<PlaceOrderResponseDto> placeOrder(@PathVariable("id") Long productId) {
         User loggedUser = authenticationService.getTokenUser();
         Product product = productService.findProductById(productId);
+        if(product == null) {
+            return ResponseEntity.notFound().build();
+        }
 
         Order order = Order.builder()
                 .product(product)
