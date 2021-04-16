6933d086608d292b8a472712257572fa379b002f
Serghei Sergheev <47123121+sergheevdev@users.noreply.github.com>
2021-04-17 00:33:50 +0200

    Fixed bash docker build script

--- docker/run.sh
+++ docker/run.sh
@@ -15,7 +15,7 @@ apt-get install maven docker.io docker-compose -y
 
 mvn clean install
 
-BUILD_FILENAME="$BASE/backend/target/$BUILD_FILENAME/marketplace-0.0.1-SNAPSHOT.jar"
+BUILD_FILENAME="$BASE/backend/target/marketplace-0.0.1-SNAPSHOT.jar"
 DEST_FILENAME="$BASE/docker/marketplace.jar"
 
 # Ensure old compiled file does not exist
