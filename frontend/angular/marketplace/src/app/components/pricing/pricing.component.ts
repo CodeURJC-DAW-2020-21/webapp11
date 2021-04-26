f282c209702223e529c7221131c7457b5e2b48f0
Serghei Sergheev <47123121+sergheevdev@users.noreply.github.com>
2021-04-26 20:14:31 +0200

    Added order placing from pricing

--- frontend/angular/marketplace/src/app/components/pricing/pricing.component.ts
+++ frontend/angular/marketplace/src/app/components/pricing/pricing.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {ProductService} from '../../services/product.service';
 import {Product} from '../../models/product.model';
 import {Error} from '../../models/error.model';
+import {OrderService} from '../../services/order.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-pricing',
@@ -20,6 +22,8 @@ export class PricingComponent implements OnInit {
 
   constructor(
     private productService: ProductService,
+    private orderService: OrderService,
+    private router: Router
   ) { }
 
   ngOnInit(): void {
@@ -44,25 +48,39 @@ export class PricingComponent implements OnInit {
   }
 
   loadDefaultState(): void {
-    if (this.categoryToProducts.size === 0) { return; }
+    if (this.categoryToProducts.size === 0) {
+      return;
+    }
     this.selectedCategory = this.categoryToProducts.keys().next().value;
     this.switchToCategory(this.selectedCategory);
   }
 
   switchToCategory(category: string): void {
-    if (!this.categoryToProducts.has(category)) { return; }
+    if (!this.categoryToProducts.has(category)) {
+      return;
+    }
     this.selectedCategory = category;
     this.selectedProduct = this.categoryToProducts.get(category)[0];
     this.selectedProductIndex = 0;
     this.lastProductIndex = this.categoryToProducts.get(category).length - 1;
-    console.log(this.selectedCategory);
   }
 
   selectProduct(): void {
     const productIndex = this.selectedProductIndex;
-    console.log(productIndex);
     this.selectedProductIndex = productIndex;
     this.selectedProduct = this.categoryToProducts.get(this.selectedCategory)[productIndex];
+  }
+
+  purchaseProduct(): void {
+    const currentProduct = this.categoryToProducts.get(this.selectedCategory)[this.selectedProductIndex];
+    const observable = this.orderService.placeOrder(currentProduct.id);
+    observable.subscribe((response) => {
+      if (response instanceof Error) {
+        this.errorMessage = response.message;
+      } else {
+        this.router.navigate(['/services']).then();
+      }
+    });
   }
 
 }
---
+++ frontend/angular/marketplace/src/app/services/order.service.ts
@@ -0,0 +1,44 @@
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {Error} from '../models/error.model';
+import {TokenService} from './token.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class OrderService {
+
+  private BASE_ROUTE = 'https://localhost:8443/api/orders';
+
+  constructor(
+    private httpClient: HttpClient,
+    private tokenService: TokenService
+  ) { }
+
+  /**
+   * Places an order for given product id.
+   * @param productId the identified correspondent to the product that is about to be bought
+   * @return observable an observable result of a boolean representing the correct order placement, an error otherwise
+   */
+  placeOrder(productId: number): Observable<boolean | Error> {
+    return new Observable<boolean | Error>((subscriber) => {
+      const requestBody = { product_id: productId };
+      const requestOptions = { headers: new HttpHeaders({ Authorization: this.tokenService.getToken() }) };
+      this.httpClient.post<any>(
+        this.BASE_ROUTE, requestBody, requestOptions)
+        .subscribe(
+          () => {
+            subscriber.next(true);
+          },
+          (errorResponse) => {
+            const responseBody = errorResponse.error;
+            // If the response body has content, then the server has answered (otherwise could not connect to server)
+            const error = 'content' in responseBody ? Error.answered(responseBody.content) : Error.unanswered();
+            subscriber.next(error);
+          }
+        );
+    });
+  }
+
+}
