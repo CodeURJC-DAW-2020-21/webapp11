064afcadfbb492d9eff4bf578e605336271c8338
Anogue <alvaronogue@gmail.com>
2021-04-30 23:38:52 +0200

    added profile component

--- frontend/angular/marketplace/src/app/components/profile/profile.component.ts
+++ frontend/angular/marketplace/src/app/components/profile/profile.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
+import {OrderService} from '../../services/order.service';
+import {UserService} from '../../services/user.service';
+import {User} from '../../models/user.model';
+import {Error} from '../../models/error.model';
 
 @Component({
   selector: 'app-profile',
@@ -7,9 +12,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+  public userDisplay: User = new User();
+
+  public password1 = '';
+  public password2 = '';
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private userService: UserService
+  ) {
+    this.route.queryParams.subscribe(params => {
+      let userId = params.id;
+      if (userId == null) {
+        userId = localStorage.getItem('user_id');
+        if (userId == null) {
+          this.router.navigate(['/login']).then();
+          return;
+        }
+      }
+      userId = Number(userId);
+      const observable = userService.findUser(userId);
+      observable.subscribe((response) => {
+        if (response instanceof Error) {
+          this.router.navigate(['/error']).then();
+        } else {
+          this.userDisplay = response;
+          console.log(response);
+        }
+      });
+    });
+  }
 
   ngOnInit(): void {
   }
+
 
 }
