28b4b85f70da8c61f1c8c5a8ae26639a13bc1717
Serghei Sergheev <47123121+sergheevdev@users.noreply.github.com>
2021-05-02 03:05:29 +0200

    Fixed spa controller

--- backend/src/main/java/es/urjc/code/daw/marketplace/web/SPAController.java
+++ backend/src/main/java/es/urjc/code/daw/marketplace/web/SPAController.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class SPAController {
     @GetMapping({"/new/**/{path:[^\\.]*}", "/{path:new[^\\.]*}"})
     public String redirect() {
-        return "forward:/new/index.html";
+        return "forward:/index.html";
     }
 }
